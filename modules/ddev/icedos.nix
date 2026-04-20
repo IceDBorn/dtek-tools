90573b777658e5bd407aac4e4f11632932cd32cd
IceDBorn <git.outsider841@simplelogin.fr>
2026-04-20 15:13:58 +0300

    rework(toolset): adapt upstream changes

--- modules/ddev/icedos.nix
+++ modules/ddev/icedos.nix
@@ -5,12 +5,11 @@
     { ... }:
     [
       (
-        {
-          lib,
-          pkgs,
-          ...
-        }:
+        { pkgs, ... }:
 
+        let
+          ddevBin = "${pkgs.ddev}/bin/ddev";
+        in
         {
           environment.systemPackages = with pkgs; [
             ddev
@@ -22,131 +21,47 @@
           ];
 
           icedos.applications.toolset.commands = [
-            (
-              let
-                inherit (lib) concatMapStrings sort;
-                ddev = "${pkgs.ddev}/bin/ddev";
-
-                colorBashHeader = ''
-                  NC='\033[0m'
-                  PURPLE='\033[0;35m'
-                  RED='\033[0;31m'
-                '';
-
-                helpFlags = ''"$1" == "" || "$1" == "--help" || "$1" == "-h" || "$1" == "help" || "$1" == "h"'';
-                purpleString = string: "\${PURPLE}${string}\${NC}";
-                redString = string: "\${RED}${string}\${NC}";
-
-                commands = [
-                  (
-                    let
-                      command = "start";
-                    in
+            {
+              command = "ddev";
+              help = "print ddev related commands";
+              commands = [
+                {
+                  command = "start";
+                  help = "start development server";
+                  script = ''
+                    ${ddevBin} poweroff
+                    ${ddevBin} start
+                    ${ddevBin} mailpit
+                  '';
+                }
+                {
+                  command = "stop";
+                  help = "stop development server";
+                  script = "${ddevBin} poweroff";
+                }
+                {
+                  command = "delete";
+                  help = "delete project from ddev";
+                  script = "${ddevBin} delete --omit-snapshot";
+                }
+                {
+                  command = "db";
+                  help = "database related commands";
+                  commands = [
                     {
-                      inherit command;
-
-                      bin = "${pkgs.writeShellScript command ''
-                        ${ddev} poweroff
-                        ${ddev} start
-                        ${ddev} mailpit
-                      ''}";
-
-                      help = "start development server";
+                      command = "import";
+                      help = "import database from file";
+                      script = ''${ddevBin} import-db --file="$1"'';
                     }
-                  )
-                  (
-                    let
-                      command = "stop";
-                    in
                     {
-                      inherit command;
-                      bin = "${pkgs.writeShellScript command "${ddev} poweroff"}";
-                      help = "stop development server";
+                      command = "info";
+                      help = "print database info";
+                      script = "${ddevBin} describe | grep --color=never db";
                     }
-                  )
-                  (
-                    let
-                      command = "delete";
-                    in
-                    {
-                      inherit command;
-                      bin = "${pkgs.writeShellScript command "${ddev} delete --omit-snapshot"}";
-                      help = "delete project from ddev";
-                    }
-                  )
-                  (
-                    let
-                      command = "db";
-                    in
-                    {
-                      inherit command;
-
-                      bin = "${pkgs.writeShellScript command ''
-                        ${colorBashHeader}
-
-                        if [[ ${helpFlags} ]]; then
-                          echo "Available commands:"
-                          echo -e "> ${purpleString "import"}: import database from file"
-                          echo -e "> ${purpleString "info"}: print database info"
-                          exit 0
-                        fi
-
-                        case "$1" in
-                          import)
-                            ${ddev} import-db --file="$2"
-                            exit 0
-                            ;;
-                          info)
-                            ${ddev} describe | grep --color=never db
-                            exit 0
-                            ;;
-                          *|-*|--*)
-                            echo -e "${redString "Unknown arg"}: $1" >&2
-                            exit 1
-                            ;;
-                        esac
-                      ''}";
-
-                      help = "database related commands";
-                    }
-                  )
-                ];
-
-                command = "ddev";
-              in
-              {
-                inherit command;
-
-                bin = "${pkgs.writeShellScript command ''
-                  ${colorBashHeader}
-
-                  if [[ ${helpFlags} ]]; then
-                    echo "Available commands:"
-
-                    ${concatMapStrings (tool: ''
-                      echo -e "> ${purpleString tool.command}: ${tool.help} "
-                    '') (sort (a: b: a.command < b.command) commands)}
-
-                    exit 0
-                  fi
-
-                  case "$1" in
-                    ${concatMapStrings (tool: ''
-                      ${tool.command})
-                        shift
-                        exec ${tool.bin} "$@"
-                        ;;
-                    '') commands}
-                    *|-*|--*)
-                      echo -e "${redString "Unknown arg"}: $1" >&2
-                      exit 1
-                      ;;
-                  esac
-                ''}";
-
-                help = "print ddev related commands";
-              }
-            )
+                  ];
+                }
+              ];
+            }
           ];
         }
       )
