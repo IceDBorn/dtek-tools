fe9aa574cb00622a28a1e7509a81a99af241e852
IceDBorn <git.outsider841@simplelogin.fr>
2026-05-11 14:47:34 +0300

    update(opencart-mcp): 0.3.0 -> 0.4.0

--- modules/opencart-mcp/icedos.nix
+++ modules/opencart-mcp/icedos.nix
@@ -9,30 +9,7 @@
         { pkgs, ... }:
 
         let
-          opencart-mcp-pkg = pkgs.python3Packages.buildPythonPackage {
-            pname = "opencart-mcp";
-            version = "0.3.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "chrisbray85";
-              repo = "opencart-mcp";
-              rev = "c2179df";
-              sha256 = "0jlrd9b0n3zvciydc75y6lb6fkpmrhazvw3pa2bygr8cy9051lgg";
-            };
-
-            patches = [ ./ddev-support.patch ];
-
-            format = "pyproject";
-            nativeBuildInputs = [ pkgs.python3Packages.setuptools ];
-
-            propagatedBuildInputs = with pkgs.python3Packages; [
-              fastmcp
-              paramiko
-              python-dotenv
-            ];
-
-            doCheck = false;
-          };
+          opencart-mcp-pkg = pkgs.callPackage ./package.nix { };
 
           pythonEnv = pkgs.python3.withPackages (ps: [ opencart-mcp-pkg ]);
         in
